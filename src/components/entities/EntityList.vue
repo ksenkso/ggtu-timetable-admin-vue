e6d91fa63af35bac0204ad15424f21f99e87ef31
ksenkso <yazunya@gmail.com>
2020-09-10 19:46:39 +0300

    feature: EntityList styles

--- src/components/entities/EntityList.vue
+++ src/components/entities/EntityList.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="entity-list">
     <li v-for="entity in entities" :key="entity.id" class="entity-list__item">
-      <router-link class="entity-list__name" :to="`${editRoute}/${entity.id}`">{{ entity.name }}</router-link>
+      <router-link class="entity-list__link" :to="`${editRoute}/${entity.id}`">{{ entity.name }}</router-link>
       <Button class="entity-list__delete" theme="danger" @click="deleteEntity(entity)">Удалить</Button>
     </li>
   </ul>
@@ -36,6 +36,32 @@ export default class EntityList extends Vue {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="sass">
+@import "../../assets/variables"
+@import "../../assets/mixins"
+.entity-list
+  list-style-type: none
+  padding: 0
+  margin: 0
+  border: 1px solid #ddd
+  border-radius: 3px
+  &__item
+    border-bottom: 1px solid #ddd
+    padding: 8px 12px
+    display: flex
+    align-items: baseline
+    align-content: baseline
+    &:hover
+      background-color: #eee
+    .button
+      margin-left: auto
+    &:last-child
+      border-bottom: none
+  &__link
+    text-decoration: none
+    cursor: pointer
+    color: map-get($colors, "dark")
+    position: relative
+    width: 100%
+    margin-right: 1em
 </style>
