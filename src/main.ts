f651652a8b763a8967b16aed5e34184f648168fa
ksenkso <yazunya@gmail.com>
2020-09-24 22:55:45 +0300

    refactor: provide Button component everywhere

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import Alert from "@/components/common/Alert.vue";
+import Button from "@/components/common/Button.vue";
 
 Vue.config.productionTip = false
 Vue.component('Alert', Alert);
+Vue.component('Button', Button);
 new Vue({
   router,
   store,
