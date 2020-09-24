ae5f7174dfe68446be997178865221055a50b11f
ksenkso <yazunya@gmail.com>
2020-09-24 22:58:21 +0300

    feature: MockLesson now provides a button to set a lesson which triggers a `set` event

--- src/components/timetable/MockLesson.vue
+++ src/components/timetable/MockLesson.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="mock-lesson">
-    <Alert theme="warning">No lesson here</Alert>
+    <Alert theme="warning">
+      No lesson here
+      <Button @click.native="$emit('set')" theme="warning">Поставить пару</Button>
+    </Alert>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import {Component, Vue} from 'vue-property-decorator';
 
 @Component({
-    name: 'MockLesson'
+  name: 'MockLesson'
 })
 export default class MockLesson extends Vue {
 
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="sass">
+.button
+  float: right
+  vertical-align: middle
 </style>
