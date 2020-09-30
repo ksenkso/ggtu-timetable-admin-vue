67efcae7c02cc659218a153b62577fcd332fc692
ksenkso <yazunya@gmail.com>
2020-09-30 18:06:05 +0300

    fix: use `SET_LOADED` mutation when fetching all entities

--- src/store/entities/actions.ts
+++ src/store/entities/actions.ts
@@ -1,9 +1,10 @@
-import {ActionTree} from "vuex";
-import {EntitiesState, NamedEntity} from "@/store/entities/types";
-import {RootState} from "@/store/types";
-import {api} from "@/api";
-import {CREATE_ENTITY, DELETE_ENTITY, GET_ALL_ENTITIES, UPDATE_ENTITY} from "@/store/entities/action-types";
-import {ADD_ENTITY, REMOVE_ENTITY, SET_ALL_ENTITIES, SET_ENTITY} from "@/store/entities/mutations-types";
+import { ActionTree } from 'vuex';
+import { EntitiesState, NamedEntity } from '@/store/entities/types';
+import { RootState } from '@/store/types';
+import { api } from '@/api';
+import { CREATE_ENTITY, DELETE_ENTITY, GET_ALL_ENTITIES, UPDATE_ENTITY } from '@/store/entities/action-types';
+import { ADD_ENTITY, REMOVE_ENTITY, SET_ALL_ENTITIES, SET_ENTITY } from '@/store/entities/mutations-types';
+import { SET_LOADED } from '@/store/entities/mutations-types';
 import { EntityType, WithId } from 'ggtu-timetable-api-client';
 
 
@@ -14,8 +15,10 @@ const createActions = (type: EntityType, overrides: ActionTree<EntitiesState, Ro
                 .then(created => context.commit(ADD_ENTITY, created));
         },
         [GET_ALL_ENTITIES](context) {
+            context.commit(SET_LOADED, false);
             return api.getEndpoint(type).getAll()
-                .then(entities => context.commit(SET_ALL_ENTITIES, entities));
+                .then(entities => context.commit(SET_ALL_ENTITIES, entities))
+                .then(() => context.commit(SET_LOADED, true));
         },
         [UPDATE_ENTITY](context, entity: WithId<NamedEntity>) {
             return api.getEndpoint(type).update(entity.id, entity)
