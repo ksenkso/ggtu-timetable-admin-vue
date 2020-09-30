78b2675c2c809d6b172610e13dbbb759602d754a
ksenkso <yazunya@gmail.com>
2020-09-30 18:36:19 +0300

    cleanup: regular timetable

--- src/router/timetable.ts
+++ src/router/timetable.ts
@@ -1,18 +1,10 @@
-import {RouteConfig} from "vue-router";
+import { RouteConfig } from 'vue-router';
 
 export default [
     {
         path: '/timetable/:groupId/edit',
         name: 'TimetableForm',
         component: () => import(/* webpackChunkName: "timetableForm" */'../views/timetables/TimetableForm.vue'),
-        meta: {
-            private: true
-        }
-    },
-    {
-        path: '/timetable/:groupId',
-        name: 'TimetableForGroup',
-        component: () => import(/* webpackChunkName: "timetableForGroup" */'../views/timetables/RegularTimetable.vue'),
         meta: {
             private: true
         }
