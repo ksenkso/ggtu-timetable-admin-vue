7d39187d4602b0c6c241739aed7231498d5091ed
ksenkso <yazunya@gmail.com>
2020-10-10 12:46:06 +0300

    refactor: made `cabinetName` global

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import router from './router'
 import store from './store'
 import Alert from "@/components/common/Alert.vue";
 import Button from "@/components/common/Button.vue";
+import {Cabinet} from "ggtu-timetable-api-client";
 
 Vue.config.productionTip = false
 Vue.component('Alert', Alert);
 Vue.component('Button', Button);
+
+Vue.filter('cabinetName', (cabinet: Cabinet) => {
+  return cabinet.building
+      ? cabinet.building.name + ', ' + cabinet.name
+      : cabinet.name;
+})
 new Vue({
   router,
   store,
