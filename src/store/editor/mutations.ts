f7e0c553d1466d3cffbed125aaafcec9dbf42ecf
ksenkso <yazunya@gmail.com>
2020-10-15 01:02:44 +0300

    feature: use new shape of api response for timetables

--- src/store/editor/mutations.ts
+++ src/store/editor/mutations.ts
@@ -1,19 +1,23 @@
-import { MutationTree } from 'vuex';
+import {MutationTree} from 'vuex';
 import {
     ADD_LESSON,
-    ADD_PATCH, REMOVE_LESSON,
+    ADD_PATCH,
+    REMOVE_LESSON,
     SET_DAY,
-    SET_GROUP_ID, SET_LESSON, SET_LESSON_TO_UPDATE,
-    SET_LESSONS_FOR_WEEK, SET_PATCH, SET_PATCH_TO_UPDATE,
+    SET_GROUP,
+    SET_GROUP_ID,
+    SET_LESSON,
+    SET_LESSON_TO_UPDATE,
+    SET_LESSONS_FOR_WEEK,
+    SET_PATCH,
+    SET_PATCH_TO_UPDATE,
     SET_PATCHES_FOR_WEEK,
     SET_WEEK
 } from '@/store/editor/mutations-types';
-import { Day, Lesson, Patch, Week } from 'ggtu-timetable-api-client';
-import { EditorState } from '@/store/editor/types';
-import { v4 } from 'uuid';
-import { REMOVE_PATCH } from '@/store/editor/action-types';
-import { SET_GROUP } from '@/store/editor/mutations-types';
-import { Group } from 'ggtu-timetable-api-client';
+import {Day, Group, Lesson, Patch, Week} from 'ggtu-timetable-api-client';
+import {EditorState, TimetableWeek} from '@/store/editor/types';
+import {v4} from 'uuid';
+import {REMOVE_PATCH} from '@/store/editor/action-types';
 
 export default {
     [SET_GROUP](state, group: Group) {
@@ -28,46 +32,13 @@ export default {
     [SET_GROUP_ID](state, groupId: number) {
         state.groupId = groupId;
     },
-    [SET_LESSONS_FOR_WEEK](state, { lessons, week }: { lessons: Lesson[]; week: Week }) {
-        // use strings as keys because Day values may change in the future and objects have string keys anyway
-        const days: Record<string, { id: string; lesson?: Lesson }[]> = {
-            [Day.Monday]: [],
-            [Day.Tuesday]: [],
-            [Day.Wednesday]: [],
-            [Day.Thursday]: [],
-            [Day.Friday]: [],
-            [Day.Saturday]: [],
-        };
-        lessons.reduce((days, lesson) => {
-            days[lesson.day].push({
-                id: v4(),
-                lesson,
-            });
-            return days;
-        }, days);
-        for (const dayIndex in days) {
-            if (!Object.prototype.hasOwnProperty.call(days, dayIndex)) {
-                continue;
-            }
-            const day = days[dayIndex];
-            day.sort((a, b) => (a.lesson as Lesson).index - (b.lesson as Lesson).index);
-            /**
-             * The main idea: go through subjects, if lesson's index is not equal to expected lesson index,
-             * then there is no lesson defined, so only set an id;
-             * increment expected index every iteration, go to next lesson only if indices are equal.
-             */
-            let expectedSerialNumber = 0, lessonIndex = 0;
-            while (lessonIndex < day.length) {
-                // current lesson is not the lesson we are looking for, so there is no lesson for current index
-                if ((day[lessonIndex].lesson as Lesson).index !== expectedSerialNumber) {
-                    day.splice(expectedSerialNumber, 0, {id: v4()});
-                } else {
-                    lessonIndex++;
-                }
-                expectedSerialNumber++;
-            }
-        }
-        state.currentTimetable[week] = days;
+    [SET_LESSONS_FOR_WEEK](state, {lessons, week}: { lessons: Record<string, (Lesson | null)[]>; week: Week }) {
+
+        state.currentTimetable[week] = Object.keys(lessons)
+            .reduce<TimetableWeek>((acc, day) => {
+                acc[day] = lessons[day].map((lesson) => ({id: v4(), lesson}));
+                return acc;
+            }, {});
     },
     [SET_PATCHES_FOR_WEEK](state, patches: Patch[]) {
         state.patches = patches.map(patch => ({id: v4(), patch}));
