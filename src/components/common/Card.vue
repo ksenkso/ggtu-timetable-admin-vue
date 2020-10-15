d830d7c53f5cfb6280a4cc92cbf4992c19b8a452
ksenkso <yazunya@gmail.com>
2020-10-15 19:11:09 +0300

    feature: removed overflow from card, added themes

--- src/components/common/Card.vue
+++ src/components/common/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div :class="className">
     <h2 class="card__header">
       <slot name="header"></slot>
     </h2>
@@ -13,41 +13,71 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from "vue-property-decorator";
+import { Component, Vue } from 'vue-property-decorator';
+import { Prop } from 'vue-property-decorator';
 
 @Component({
   name: 'Card'
 })
 export default class Card extends Vue {
+  @Prop() theme?: string;
 
+  get className() {
+    const classes = ['card'];
+    if (this.theme) {
+      classes.push(`card_theme-${this.theme}`);
+    }
+    return classes;
+  }
 }
 </script>
 
 <style scoped lang="sass">
+@import '../../assets/variables'
+@import '../../assets/functions'
 .card
-  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .13)
-  overflow: hidden
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .17)
   border-radius: .3125rem
+
+  &_theme
+    @each $mod, $color in $colors
+      &-#{$mod}
+        background: rgba($color, .3)
+        color: #000
+
+        .card__header
+          border-color: $color
+
   &__header
     border-bottom: 1px solid #ddd
+
   &__header, &__footer
     padding: .5rem 1rem
+
     &:empty
       display: none
+
   &__content
     padding: 1em
+
   &__row
     margin-bottom: 1rem
+
     &:last-child
       margin-bottom: 0
+
   &_hover
     transition: box-shadow .3s ease-in-out
+
     &:hover
-      box-shadow: 0 2px 7px 0 rgba(0, 0, 0, .17)
+      box-shadow: 0 2px 7px 0 rgba(0, 0, 0, .21)
+
   h1, h2, h3
     margin: 0
+
   p
     margin-top: .5rem
+
     &:last-child
       margin-bottom: 0
 
