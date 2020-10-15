80a9f3075a677dcbe2f480afb60f261de783fb00
ksenkso <yazunya@gmail.com>
2020-10-15 19:07:45 +0300

    refactor: filter-callback -> set-filter-value, added custom filter to compare computed cabinet names

--- src/views/cabinets/Cabinets.vue
+++ src/views/cabinets/Cabinets.vue
@@ -2,7 +2,7 @@
   <Page title="Кабинеты">
     <EntityControls
         create-route="/cabinets/create"
-        :filter-callback="filterCallback"
+        :set-filter-value="setFilterValue"
     >
     </EntityControls>
     <Alert v-if="isEmpty && dataLoader.loaded" theme="warning">Записи не найдены</Alert>
@@ -24,21 +24,24 @@
 
 <script lang="ts">
 import Page from '@/views/Page.vue';
-import {Component} from "vue-property-decorator";
-import {namespace} from 'vuex-class';
-import EntityList from "@/components/entities/EntityList.vue";
-import EntityControls from "@/components/common/EntityControls.vue";
-import entityListPage from "@/mixins/EntityListPage";
-import {GET_ALL_ENTITIES} from "@/store/entities/action-types";
-import {NamedEntityDict} from "@/store/entities/types";
-import {Store} from "vuex";
-import {RootState} from "@/store/types";
-import {AbstractDataLoader} from "@/views/cabinets/DataLoader";
+import { Component } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+import EntityList from '@/components/entities/EntityList.vue';
+import EntityControls from '@/components/common/EntityControls.vue';
+import entityListPage from '@/mixins/EntityListPage';
+import { GET_ALL_ENTITIES } from '@/store/entities/action-types';
+import { NamedEntityDict } from '@/store/entities/types';
+import { Store } from 'vuex';
+import { RootState } from '@/store/types';
+import { AbstractDataLoader } from '@/views/cabinets/DataLoader';
+import { EntitiesState } from '@/store/entities/types';
+import { Cabinet } from 'ggtu-timetable-api-client';
+import { Building } from 'ggtu-timetable-api-client';
 
 const cabinets = namespace('cabinets');
 const buildings = namespace('buildings');
 
-class CabinetsLoader extends AbstractDataLoader{
+class CabinetsLoader extends AbstractDataLoader {
 
   constructor(store: Store<RootState>) {
     super(store);
@@ -58,6 +61,15 @@ export default class Cabinets extends entityListPage(cabinets, CabinetsLoader) {
 
   @buildings.State('entities') buildings!: NamedEntityDict;
   @buildings.Getter('isEmpty') isBuildingsEmpty!: boolean;
+
+  filter(state: EntitiesState) {
+    return (id: number) => {
+      const cabinet = state.entities[id] as Cabinet;
+      const building = cabinet.building as Building;
+      const name = `${building.name.split(' ')[1]}к${cabinet.number}`;
+      return name.toLowerCase().includes(state.filterValue);
+    };
+  }
 }
 </script>
 
